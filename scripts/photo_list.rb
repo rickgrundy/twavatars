772ede111dd59d4e37eee0da8f67eb1a6e17fb9e
Rick Grundy <rick@rickgrundy.com>
2013-02-06 11:24:24 +1100

    Photo list script generates a single montage image in addition to the txt file listing filenames

--- scripts/photo_list.rb
+++ scripts/photo_list.rb
@@ -1,7 +1,29 @@
+require 'fileutils'
+require 'RMagick'
+
 ask = ->(prompt){ puts prompt; gets.strip }
 
-dir = ask['Photos directory:']
+W = 460
+H = 410
 
-File.open('photos.txt', 'w') do |txt| 
-  Dir["#{dir}/*"].map { |f| txt << File.basename(f) + "\n" }
+output_dir = FileUtils.mkdir_p('tmp').first
+photos_dir = ask['Photos directory:']
+photos_dir = "/Users/rick/avatars" if photos_dir.empty?
+
+files = Dir["#{photos_dir}/*.{jpg,jpeg,png,JPG,JPEG,PNG}"]
+
+File.open("#{output_dir}/photos.txt", 'w') do |txt| 
+  txt.puts files.map { |f| File.basename f }
 end
+
+tiles = Magick::ImageList.new *files
+tiles.each { |i| i.resize! W, H }
+montage = tiles.montage do
+  self.border_width = 0
+  self.geometry = Magick::Geometry.new W, H, 0, 0
+  self.tile = Magick::Geometry.new tiles.size, 1
+end
+montage.write "#{output_dir}/avatars.jpg"
+
+
+puts "--> Finished. Upload #{output_dir}/avatars.jpg to the CDN and #{output_dir}/photos.txt to the app."
